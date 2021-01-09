d5c3ec3b723dc511471d57c8dd1221be1024725a
badre429 <badre429@hotmail.com>
2021-01-09 18:26:16 +0100

    feat:(localication) add core localisation file

--- aspnet-core/Core/AbpDzCoreModule.cs
+++ aspnet-core/Core/AbpDzCoreModule.cs
@@ -1,17 +1,20 @@
 ﻿using System;
+using AbpDz.Localization;
 using Volo.Abp.Account;
 using Volo.Abp.AutoMapper;
 using Volo.Abp.FeatureManagement;
 using Volo.Abp.Identity;
+using Volo.Abp.Localization;
+using Volo.Abp.Localization.ExceptionHandling;
 using Volo.Abp.Modularity;
 using Volo.Abp.PermissionManagement;
 using Volo.Abp.TenantManagement;
+using Volo.Abp.VirtualFileSystem;
 
 namespace AbpDz.Core
 {
     [DependsOn(
-    typeof(AbpAccountApplicationModule),
-    typeof(AbpIdentityApplicationModule),
+     
     typeof(AbpPermissionManagementApplicationModule),
     typeof(AbpTenantManagementApplicationModule),
     typeof(AbpFeatureManagementApplicationModule)
@@ -24,6 +27,27 @@ namespace AbpDz.Core
             {
                 options.AddMaps<AbpDzCoreModule>();
             });
+
+            Configure<AbpVirtualFileSystemOptions>(options =>
+            {
+                options.FileSets.AddEmbedded<AbpDzCoreModule>();
+            });
+
+            Configure<AbpLocalizationOptions>(options =>
+            {
+                options.Resources
+                    .Add<AbpDzResource>("en")
+                    // .AddBaseTypes(typeof(AbpValidationResource))
+                    .AddVirtualJson("/Localization/AbpDz");
+
+                options.DefaultResourceType = typeof(AbpDzResource);
+            });
+
+            Configure<AbpExceptionLocalizationOptions>(options =>
+            {
+                options.MapCodeNamespace("AbpDz", typeof(AbpDzResource));
+            });
         }
+
     }
 }
