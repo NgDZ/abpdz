efa02628b37a32da5a4be2543b1d02bc890a063d
badre429 <badre429@hotmail.com>
2021-01-15 13:23:46 +0100

    dto(EventFilterDto)

---
+++ aspnet-core/Core/Dto/EventFilterDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Volo.Abp.Application.Dtos;
+
+namespace AbpDz.Core
+{
+    public class EventFilterDto : PagedAndSortedResultRequestDto
+    {
+
+#pragma warning disable  0114
+        public int MaxResultCount { get; set; }
+
+        public string Filter { get; set; }
+
+        public string Id { get; set; }
+        public string Type { get; set; }
+        public Guid? Source { get; set; }
+        public Guid? Creator { get; set; }
+        public Guid? UserId { get; set; }
+
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int? Code { get; set; }
+        public int? State { get; set; }
+        public int? Severity { get; set; }
+        public bool? Checked { get; set; }
+    }
+
+}
