2e9394837dc1ed1aa5e5a4e784ef490bd1a3f5d8
badre429 <badre429@hotmail.com>
2021-01-15 13:28:12 +0100

    api(AbpDzNotificationController) get all action

--- aspnet-core/Notifications/Controllers/AbpDzNotificationController.cs
+++ aspnet-core/Notifications/Controllers/AbpDzNotificationController.cs
@@ -1,14 +1,19 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AbpDz.Core;
 using AbpDz.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Primitives;
 using Volo.Abp;
+using Volo.Abp.Application.Dtos;
 using Volo.Abp.AspNetCore.Mvc;
 using Volo.Abp.Data;
+using Volo.Abp.Domain.Repositories;
 using Volo.Abp.EntityFrameworkCore.Modeling;
 
 
@@ -17,6 +22,7 @@ namespace AbpDz.Notifications
 
     [RemoteService]
     [ApiExplorerSettings(GroupName = "Notification")]
+    [Route("api/abpdz-notification/[action]")]
     public class AbpDzNotificationController : AbpController, Volo.Abp.DependencyInjection.ITransientDependency
     {
         public AbpDzNotificationController(
@@ -28,15 +34,59 @@ namespace AbpDz.Notifications
 
         public AbpDzNotificationService NotificationService { get; }
 
-        [HttpGet("/api/abpdz-notification/register")]
-        public async Task RegisterClient()
+        [HttpGet()]
+        public async Task Register()
         {
 
             await this.NotificationService.RegisterClient();
 
         }
 
-        [HttpGet("/api/abpdz-notification/ping")]
+        [HttpGet]
+        public async Task<PagedResultDto<AbpDzNotificationInfo>> GetAll(EventFilterDto filter)
+        {
+            var repo = this.ServiceProvider.GetService(typeof(IRepository<AbpDzNotificationInfo, Guid>)) as IRepository<AbpDzNotificationInfo, Guid>;
+
+            var Config = this.ServiceProvider.GetService(typeof(AbpDzNotificationServiceConfig)) as AbpDzNotificationServiceConfig;
+            var query = repo.AsQueryable();
+            var permissions = new List<string>();
+
+
+
+            if (CurrentUser.Id != filter.UserId && !await AuthorizationService.IsGrantedAsync("AbpIdentity.Users.Create"))
+            {
+                filter.UserId = CurrentUser.Id;
+            }
+            foreach (var item in Config.NotifyPermissions)
+            {
+                if (await this.AuthorizationService.IsGrantedAsync(item.Key))
+                {
+                    permissions.Add(item.Key);
+                }
+            }
+
+            if (permissions.Count > 0)
+            {
+                query = query.Where(k => permissions.Contains(k.RecipientPermission));
+            }
+            if (filter.UserId.HasValue)
+            {
+                query = query.Where(k => k.RecipientId == filter.UserId);
+            }
+            if (filter.StartDate.HasValue)
+            {
+                query = query.Where(k => k.CreationTime >= filter.StartDate);
+            }
+            if (filter.EndDate.HasValue)
+            {
+                query = query.Where(k => k.CreationTime <= filter.EndDate);
+            }
+            return new PagedResultDto<AbpDzNotificationInfo>(
+                await query.CountAsync(),
+                await query.ToListAsync()
+            );
+        }
+        [HttpGet()]
         public async Task Ping()
         {
 
