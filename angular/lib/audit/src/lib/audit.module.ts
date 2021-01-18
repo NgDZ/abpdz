d66c23084f04394251d294004faeebb80b38cf9b
badre429@hotmail.com <badre429@hotmail.com>
2021-01-18 14:43:13 +0100

    wip(audit) package inital structure

---
+++ angular/lib/audit/src/lib/audit.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { AuditComponent } from './audit.component';
+
+
+
+@NgModule({
+  declarations: [AuditComponent],
+  imports: [
+  ],
+  exports: [AuditComponent]
+})
+export class AuditModule { }
