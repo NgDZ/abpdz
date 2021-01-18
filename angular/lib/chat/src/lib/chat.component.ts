14ea16af642f8875d4c0f8bcef13e7d54c12706e
badre429@hotmail.com <badre429@hotmail.com>
2021-01-18 14:43:25 +0100

    wip(chat) package inital structure

---
+++ angular/lib/chat/src/lib/chat.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-chat',
+  template: `
+    <p>
+      chat works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ChatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ angular/lib/chat/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of chat
+ */
+
+export * from './lib/chat.service';
+export * from './lib/chat.component';
+export * from './lib/chat.module';
