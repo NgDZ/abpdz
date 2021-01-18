8cba71921d35a9823e7508da060e9aed29dfe180
badre429@hotmail.com <badre429@hotmail.com>
2021-01-18 15:38:35 +0100

    wip(cms) intial file

---
+++ angular/lib/cms/src/lib/cms.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-cms',
+  template: `
+    <p>
+      cms works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class CmsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
