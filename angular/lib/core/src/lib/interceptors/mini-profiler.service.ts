a6a0ee4d53e50588a1c911e6df2a9c177998e51a
badre429@hotmail.com <badre429@hotmail.com>
2021-02-02 09:13:08 +0100

    feat miniprofoiler ectivate by localstore keyvalue

--- angular/lib/core/src/lib/interceptors/mini-profiler.service.ts
+++ angular/lib/core/src/lib/interceptors/mini-profiler.service.ts
@@ -1,28 +1,27 @@
-import { Injectable, isDevMode, NgZone } from '@angular/core';
-
 import {
+  HttpEvent,
+  HttpHandler,
+  HttpHeaders,
   HttpInterceptor,
   HttpRequest,
-  HttpHandler,
-  HttpEvent,
   HttpResponse,
-  HttpHeaders,
   HttpXsrfTokenExtractor,
 } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
-
-import { debounceTime, skipWhile, tap } from 'rxjs/operators';
+import { Injectable, isDevMode } from '@angular/core';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class MiniProfilerInterceptor implements HttpInterceptor {
-  static channel: BroadcastChannel;
-  constructor(private ngZone: NgZone) {
-    if (isDevMode() && MiniProfilerInterceptor.channel == null) {
-      MiniProfilerInterceptor.channel = new BroadcastChannel(
-        'mini-profiler-channel'
+  constructor() {
+    if (isDevMode()) {
+      console.log(
+        "to enable miniprofiler localStorage.setItem('USE_MINIPROFILER','true')"
       );
     }
   }
+  static loaded = false;
+  static id = 0;
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
@@ -30,21 +29,61 @@ export class MiniProfilerInterceptor implements HttpInterceptor {
     if (!isDevMode()) {
       return next.handle(req);
     }
-
+    if (localStorage.getItem('USE_MINIPROFILER') != 'true') {
+      return next.handle(req);
+    }
+    if (MiniProfilerInterceptor.loaded === false) {
+      MiniProfilerInterceptor.loaded = true;
+      const s = document.createElement('script');
+      s.setAttribute('src', '/profiler/includes.min.js');
+      s.setAttribute('id', 'mini-profiler');
+      s.setAttribute('async', 'async');
+      s.setAttribute('data-path', '/profiler/');
+      s.setAttribute('data-ids', '');
+      s.setAttribute('data-current-id', '');
+      s.setAttribute('data-position', 'BottomLeft');
+      s.setAttribute('data-scheme', 'Auto');
+      s.setAttribute('data-authorized', 'true');
+      s.setAttribute('data-controls', 'true');
+      s.setAttribute('data-children', 'true');
+      // s.setAttribute('data-trivial', 'true');
+      s.setAttribute('data-max-traces', '16');
+      s.setAttribute('data-toggle-shortcut', 'Alt+h');
+      s.setAttribute('data-trivial-milliseconds', '2.0');
+      s.setAttribute(
+        'data-ignored-duplicate-execute-types',
+        'Open,OpenAsync,Close,CloseAsync'
+      );
+      document.body.appendChild(s);
+    }
     return next.handle(req).pipe(
       tap((evt) => {
-        if (
-          evt instanceof HttpResponse &&
-          evt?.headers?.getAll('x-miniprofiler-ids')
-        ) {
-          this.ngZone.runOutsideAngular(() => {
-            MiniProfilerInterceptor.channel.postMessage(
-              evt?.headers?.getAll('x-miniprofiler-ids')
-            );
-          });
+        if (evt instanceof HttpResponse) {
+          if (
+            typeof (window as any).MiniProfiler !== 'undefined' &&
+            evt &&
+            evt.headers
+          ) {
+            this.makeMiniProfilerRequests(evt.headers);
+          }
         }
       })
     );
+  }
+
+  private makeMiniProfilerRequests(headers: HttpHeaders) {
+    try {
+      const miniProfilerHeaders = headers.getAll('x-miniprofiler-ids');
+
+      if (!miniProfilerHeaders) {
+        return;
+      }
+
+      miniProfilerHeaders.forEach((miniProfilerIdHeaderValue) => {
+        const ids = JSON.parse(miniProfilerIdHeaderValue) as string[];
+        (window as any).MiniProfiler.fetchResults(ids);
+      });
+    } catch (error) {}
   }
 }
 
